c96de8bf8de6a4d81fb66e01af459479fcd2e8fd
LoserPanda <loserpanda82@gmail.com>
2018-08-14 19:52:43 +0300

    my initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,46 @@
+// import React, {Component} from 'react';
+// import './App.css';
+// import DataList from "./Components/DataList";
+//
+// class App extends Component {
+//     state = {data: []};
+//
+//     componentDidMount() {
+//         let thisThis = this;
+//         fetch('/osoite')
+//             .then((resp) => {
+//                 return resp.json();
+//             })
+//             .then((obj) => {
+//                 thisThis.setState({data: obj});
+//                 console.log("Object", obj);
+//             })
+//
+//     };
+//
+//     deleteData = (objectId) => {
+//         fetch('/osoite/' + objectId, {
+//             method: 'DELETE'
+//         })
+//             .then(resp => {
+//                 console.log("the list after deletion", resp);
+//                 this.componentDidMount();
+//
+//             });
+//     };
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h1>RestMyCode_3.0</h1>
+//                 <DataList deleteData={this.deleteData} data={this.state.data}/>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default App;
+
 import React, {Component} from 'react';
 import './App.css';
 import DataList from "./Components/DataList";
@@ -18,22 +61,11 @@ class App extends Component {
 
     };
 
-    deleteData = (objectId) => {
-        fetch('/osoite/' + objectId, {
-            method: 'DELETE'
-        })
-            .then(resp => {
-                console.log("the list after deletion", resp);
-                this.componentDidMount();
-
-            });
-    };
-
     render() {
         return (
             <div>
                 <h1>RestMyCode_3.0</h1>
-                <DataList deleteData={this.deleteData} data={this.state.data}/>
+                <DataList data={this.state.data}/>
             </div>
         );
     }
--- src/Components/DataList.js
+++ src/Components/DataList.js
@@ -1,12 +1,47 @@
+// import React, {Component} from 'react';
+// //
+// // import Data from "./Data";
+// //
+// // class DataList extends Component {
+// //     render() {
+// //         const deleteData = this.props.deleteData;
+// //         var data = this.props.data.map(data =>
+// //             <Kayttaja deleteData={deleteData} key={data.id} data={data}/>
+// //         );
+// //         return (
+// //             <table id="datalist">
+// //                 <thead>
+// //                 <tr>
+// //                     <th>Id</th>
+// //                     <th>Title</th>
+// //                     <th>Description</th>
+// //                     <th>Date</th>
+// //                     <th>Tags</th>
+// //                     <th>Score</th>
+// //                     <th>Code</th>
+// //                     <th>Author</th>
+// //                     <th><em>DELETE</em></th>
+// //                 </tr>
+// //                 </thead>
+// //                 <tbody>
+// //                 {datalist}
+// //                 </tbody>
+// //             </table>
+// //         )
+// //     }
+// // }
+// //
+// // export default DataList;
+
 import React, {Component} from 'react';
 
 import Data from "./Data";
 
 class DataList extends Component {
     render() {
-        const deleteData = this.props.deleteData;
-        var data = this.props.data.map(data =>
-            <Kayttaja deleteData={deleteData} key={data.id} data={data}/>
+        // const deleteData = this.props.deleteData;
+        var datalist = this.props.data.map(data =>
+            <Data key={data.id} data={data}/>
         );
         return (
             <table id="datalist">
